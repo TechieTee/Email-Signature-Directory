69dfc0dcef5f289fe6e1d9d6b5132b3bb0e61d94
techietee <mailtomodupe@gmail.com>
2023-01-31 16:54:27 +0100

    adding card implementation

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map((con) => (
+        <Card
+          key={con.id}
+          name={con.name}
+          img={con.imgURL}
+          tel={con.phone}
+          email={con.email}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contacts.js
@@ -0,0 +1,37 @@
+const contacts = [
+    {
+      id: 1,
+      name: "Beyonce",
+      imgURL:
+        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+      phone: "+123 456 789",
+      email: "b@beyonce.com"
+    },
+    {
+      id: 2,
+      name: "Jack Bauer",
+      imgURL:
+        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+      phone: "+987 654 321",
+      email: "jack@nowhere.com"
+    },
+    {
+      id: 3,
+      name: "Chuck Norris",
+      imgURL:
+        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+      phone: "+918 372 574",
+      email: "gmail@chucknorris.com"
+    },
+    {
+      id: 4,
+      name: "Me",
+      imgURL:
+        "https://media.licdn.com/dms/image/C4D03AQFwZMeKkwPb_A/profile-displayphoto-shrink_400_400/0/1655409410441?e=1680739200&v=beta&t=b5WCcEhnKc6l-miCoesL0JFc_2XOqxy6w-kcFxF-sGU",
+      phone: "+2349031703106",
+      email: "modupe@gmail.com"
+    }
+  ];
+  
+  export default contacts;
+  
